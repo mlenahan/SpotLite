d29316710ef8a0bcfd2ee562ed06551ee309fed3
Michael Lenahan <michael_lenahan@hotmail.com>
2021-08-24 20:06:54 +0000

    Add some of the necessary functions for auth

--- js/authorisation.js
+++ js/authorisation.js
@@ -1,4 +1,4 @@
-var redirect_uri = "http://127.0.0.1:5500/index.html";
+var redirect_uri = "https://mlenahan.github.io/song-city/requestAuthorisation.html";
  
 
 var client_id = ""; 
@@ -7,7 +7,34 @@ var client_secret = ""; // In a real app you should not expose your client_secre
 const AUTHORIZE = "https://accounts.spotify.com/authorize"
 
 function onPageLoad() {
+    if ( window.location.search.length > 0 ){
+        handleRedirect();
+    }
+}
 
+function handleRedirect(){
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+
+function fetchAccessToken( code ){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+
+function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
 }
 
 function requestAuthorization(){
