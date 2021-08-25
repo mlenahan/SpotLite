0ea50eb467e5520aa01a6c7cf7f73f4a34ce5bc8
Michael Lenahan <michael_lenahan@hotmail.com>
2021-08-25 16:03:43 +0000

    Remove unused functions and more refactoring to existing function

---
+++ js/authorization.js
@@ -0,0 +1,53 @@
+var REDIRECT_URI = "https://8000-azure-firefly-qwnrsc9s.ws-eu16.gitpod.io/callback.html";
+ 
+
+const CLIENT_ID = "c88f76e40dab4687994225268147612c";
+const CLIENT_SECRET = "a634c74cd99a4b1a9f8e567bbd8a7c80";
+
+const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+
+function onPageLoad() {
+    /* This function is called when the page loads. We check if this is a redirect from Spotify.
+     * If it is, we fire the handleRedirect function */
+    const code = getCodeFromSearchParams();
+    if (code) {
+        handleRedirect(code);
+    }
+}
+
+function getCodeFromSearchParams() {
+    /* Get code from querystring parameter returned by Spotify.
+     * Will return null if querystring parameter is not present */
+    const urlParams = new URLSearchParams(window.location.search);
+    return code = urlParams.get('code');
+}
+
+function createRequestBody(code) {
+    /* Generate body to be sent with Authorization request */
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(REDIRECT_URI);
+    body += "&client_id=" + CLIENT_ID;
+    body += "&client_secret=" + CLIENT_SECRET;
+    return body;
+}
+
+
+function handleRedirect(code) {
+    const method = "POST";
+    const body = createRequestBody(code);
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET));
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    fetch(TOKEN_ENDPOINT, { method, headers, body }).then(handleSuccessResponse);
+}
+
+function handleSuccessResponse(response) {
+    response.json().then(handleJson);
+}
+
+function handleJson(data){
+    const { access_token, refresh_token } = data;
+    localStorage.setItem("access_token", access_token);
+    localStorage.setItem("refresh_token", refresh_token);
+}
---
+++ js/requestAuthorization.js
@@ -0,0 +1,14 @@
+const REDIRECT_URI = "https://8000-azure-firefly-qwnrsc9s.ws-eu16.gitpod.io/callback.html";
+const CLIENT_ID = "c88f76e40dab4687994225268147612c";
+const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
+
+function requestAuthorization() {
+
+    let url = AUTHORIZE_ENDPOINT;
+    url += "?client_id=" + CLIENT_ID;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(REDIRECT_URI);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+}
