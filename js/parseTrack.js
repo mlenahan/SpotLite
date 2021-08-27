2ce35a3f0da1a17834cb5ef07c21a8a625499f78
Michael Lenahan <michael_lenahan@hotmail.com>
2021-08-27 14:09:57 +0000

    Add logic to get actual song image

--- js/parseTrack.js
+++ js/parseTrack.js
@@ -1,5 +1,9 @@
 function parseTrack(track) {
-    console.log(track)
+    const album = track.album;
+    const image = album.images.find((image) => {
+        return image.width === 300;
+    })
+    const url = image.url || '';
     const songTitle = track.name;
     const artistName = track.artists[0].name;
     const songDuration = millisToMinutesAndSeconds(track.duration_ms);
@@ -7,6 +11,7 @@ function parseTrack(track) {
     return {
         songTitle,
         artistName,
-        songDuration
+        songDuration,
+        url
     };
 }
