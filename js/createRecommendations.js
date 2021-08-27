3d81c9c81738375c8971f03d486ce56b1f9dbe9c
Michael Lenahan <michael_lenahan@hotmail.com>
2021-08-27 10:40:58 +0000

    Finish off createRecommendations.js

--- js/createRecommendations.js
+++ js/createRecommendations.js
@@ -5,6 +5,7 @@ function createRecommendations() {
     const fillFormEl = document.createElement('h2');
     fillFormEl.innerText = fillForm;
 
+    // Artist Elements
     const artistLabelEl = document.createElement('label');
     artistLabelEl.setAttribute('for', 'artist');
     artistLabelEl.innerText = artistLabel;
@@ -15,6 +16,14 @@ function createRecommendations() {
     artistInputEl.setAttribute('name', 'artistname');
     artistInputEl.setAttribute('placeholder', 'Artist name..');
 
+    // Artist Divs
+    const artistLabelDiv = document.createElement('div');
+    artistLabelDiv.appendChild(artistLabelEl);
+
+    const artistInputDiv = document.createElement('div');
+    artistInputDiv.appendChild(artistInputEl);
+
+    // Song Elements
     const songLabelEl = document.createElement('label');
     songLabelEl.setAttribute('for', 'song')
     songLabelEl.innerText = songLabel;
@@ -25,6 +34,14 @@ function createRecommendations() {
     songInputEl.setAttribute('name', 'songname');
     songInputEl.setAttribute('placeholder', 'Song name..');
 
+    // Song Divs
+    const songLabelDiv = document.createElement('div');
+    songLabelDiv.appendChild(songLabelEl);
+
+    const songInputDiv = document.createElement('div');
+    songInputDiv.appendChild(songInputEl);
+
+    // Genre Elements
     const genreLabelEL = document.createElement('label');
     genreLabelEL.setAttribute('for', 'genre');
     genreLabelEl.innerText = genreLabel;
@@ -36,13 +53,44 @@ function createRecommendations() {
     genreSelectEl.setAttribute('id', 'genre');
 
     for (let i of genre) {
-        var genreOption = document.createElement("option");
+        const genreOption = document.createElement("option");
         genreOption.value = genre[i];
         genreOption.text = genre[i];
         genreSelectEl.appendChild(genreOption);
     }
 
+    // Genre Divs
+    const genreLabelDiv = document.createElement('div');
+    genreLabelDiv.appendChild(genreLabelEl);
+
+    const genreSelectDiv = document.createElement('div');
+    genreSelectDiv.setAttribute('id', 'genre');
+    genreSelectDiv.setAttribute('name', 'genre');
+    genreSelectDiv.appendChild(genreSelectEl);
+
+    // Submit button element
     const submitButtonEL = document.createElement('submit');
     submitButtonEL.setAttribute('type', 'submit');
     submitButtonEL.setAttribute('value', 'Submit')
+
+    const submitButtonDiv = document.createElement('div');
+    submitButtonDiv.appendChild(submitButtonEL);
+
+    // Adding divs to form
+    const loginForm = document.createElement('form');
+    // this form probably doesn't even need a class as not being used in css?
+    loginForm.classList.add('login-form'); 
+    loginForm.appendChild(artistLabelDiv);
+    loginForm.appendChild(artistInputDiv);
+    loginForm.appendChild(songLabelDiv);
+    loginForm.appendChild(songInputDiv);
+    loginForm.appendChild(genreLabelDiv);
+    loginForm.appendChild(genreSelectDiv);
+    loginForm.appendChild(submitButtonDiv);
+
+    const musicForm = document.createElement('div');
+    musicForm.classList.add('music-form');
+    musicForm.appendChild.add(newSongsTitleEL);
+    musicForm.appendChild.add(fillFormEl);
+    musicForm.appendChild.add(loginForm);
 }
