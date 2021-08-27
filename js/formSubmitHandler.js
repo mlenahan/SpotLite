cf38cc29aac9d80e9230bcd8b3c374f4ed8af2ab
Michael Lenahan <michael_lenahan@hotmail.com>
2021-08-27 12:33:53 +0000

    Wrap handleRecommendationsFormSubmit in other function getHandleRecommendationsFormSubmit

--- js/formSubmitHandler.js
+++ js/formSubmitHandler.js
@@ -11,27 +11,43 @@ function parseForm(form) {
     }
 }
 
-function handleRecommendationsFormSubmit(e) {
-    // Default behaviour of form submit event is to reload page. Prevent this.
-    e.preventDefault(); 
-    // e.target refers to the element that fired the submit event, i.e. the form element.
-    const form = e.target;
-    const formData = parseForm(form);
+function getHandleRecommendationsFormSubmit(setLoading, handleSuccess, handleUnauthorized, handleError) {
 
-    const recommendationsEndpoint = 'https://api.spotify.com/v1/recommendations'
-    // convert form data to query string parameters, e.g. 
-    // seed_artists=skepta&seed_tracks=shutdown&seed_genres=rap
-    const searchParams = new URLSearchParams(formData).toString();
-    const fullUrl = recommendationsEndpoint + '?' + searchParams;
+    function handleRecommendationsFormSubmit(e) {
+        // Default behaviour of form submit event is to reload page. Prevent this.
+        e.preventDefault(); 
+        // e.target refers to the element that fired the submit event, i.e. the form element.
+        const form = e.target;
+        const formData = parseForm(form);
 
-    // headers are passed with request. We need authorisation header (token) to authenticate with Spotify
-    const headers = new Headers();
-    const token = window.localStorage.getItem('access_token');
-    const Authorization = 'Bearer ' + token;
-    headers.append('Authorization', Authorization);
+        const recommendationsEndpoint = 'https://api.spotify.com/v1/recommendations'
+        // convert form data to query string parameters, e.g. 
+        // seed_artists=skepta&seed_tracks=shutdown&seed_genres=rap
+        const searchParams = new URLSearchParams(formData).toString();
+        const fullUrl = recommendationsEndpoint + '?' + searchParams;
 
-    // call the url with headers
-    return fetch(fullUrl, {
-        headers
-    })
+        // headers are passed with request. We need authorisation header (token) to authenticate with Spotify
+        const headers = new Headers();
+        const token = window.localStorage.getItem('access_token');
+        const Authorization = 'Bearer ' + token;
+        headers.append('Authorization', Authorization);
+
+        // call the url with headers
+        setLoading();
+        return fetch(fullUrl, {
+            headers
+        }).then((response) => {
+            if (response.status === 401) {
+                handleUnauthorized();
+            }
+            if (response.status === 400) {
+                handleError();
+            }
+            if (response.status === 200) {
+                handleSuccess();
+            }
+        })
+    }
+
+    return handleRecommendationsFormSubmit;
 }
