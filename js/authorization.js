811e2ec4c75569b7ac77d539d9f71f1f3732fe31
Michael Lenahan <michael_lenahan@hotmail.com>
2021-09-01 11:34:59 +0000

    Changes for deployment test

--- js/authorization.js
+++ js/authorization.js
@@ -1,4 +1,5 @@
-var REDIRECT_URI = "https://8000-azure-firefly-qwnrsc9s.ws-eu16.gitpod.io/index.html";
+// var REDIRECT_URI = "https://8000-azure-firefly-qwnrsc9s.ws-eu16.gitpod.io/index.html";
+var REDIRECT_URI = "https://mlenahan.github.io/spotlite/index.html";
  
 
 const CLIENT_ID = "c88f76e40dab4687994225268147612c";
